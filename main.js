a207302a656f81e9e2ca3e6823948b03d6fd4242
MuammadAnusAhmed <MUHAMMADANUSAHMEDON@gmail.com>
2024-04-15 17:46:22 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,2 @@
+var personname = "Muhammad Anus Ahmed";
+console.log("Hello ".concat(personname, " would you like to learn some python today"));
---
+++ main.ts
@@ -0,0 +1,2 @@
+let personname = "Muhammad Anus Ahmed"
+console.log (`Hello ${personname} would you like to learn some python today`)
